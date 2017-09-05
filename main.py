1ad10b6845faf9662fdf4cd8246068e42ddb71ec
Li-Ting Tsai <and.liting@gmail.com>
2017-09-05 23:54:42 +0800

    add pass ptrace

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
+from __future__ import print_function
+import os
 import sys
+import time
 sys.path.append("/Applications/Xcode.app/Contents/SharedFrameworks/LLDB.framework/Versions/Current/Resources/Python/")
 import lldb
 
-def handle_ptrace(frame, bp_loc, dict):
-    pass
+debugger = lldb.SBDebugger.Create()
+debugger.SetAsync(False)
 
-def anti_pt_deny_attach(debugger, command, result, internal_dict):
-    """hi"""
-    debugger.HandleCommand("b ptrace")
-    debugger.HandleCommand("breakpoint command add 1")
-    #debugger.HandleCommand("re w rdi 0")
-    #debugger.HandleCommand("c")
-    #debugger.HandleCommand("c")
-    #debugger.HandleCommand("DONE")
+def anti_pt_deny_attach(breakpoint, frame, bpno):
+    global debugger
+    print("Hit ptrace breakpoint")
+    debugger.HandleCommand("re w rdi 0")
 
-def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f main.anti_pt_deny_attach anti_pt_deny_attach')
-    print 'The "anti_pt_deny_attach" python command has been installed and is ready for use.'
-
-
+def main():
+    exe = sys.argv[1]
+    #debugger = lldb.SBDebugger.Create()
+    #debugger.SetAsync(False)
+    global debugger
+    print("Creating a target for '%s'" % exe)
+    target = debugger.CreateTargetWithFileAndArch(exe, lldb.LLDB_ARCH_DEFAULT)
+    if not target:
+        print("Failed to Create the target")
+        sys.exit(1)
+    
+    ptrace_bp = target.BreakpointCreateByName("ptrace")
+    print(ptrace_bp)
+    ptrace_bp.SetScriptCallbackFunction("anti_pt_deny_attach")
+    process = target.LaunchSimple(None, None, os.getcwd())
+    if not process:
+       print("Failed to Launch the process") 
+       sys.exit(2)
+    print(process)
+    while True:
+        if process.GetState() == lldb.eStateStopped:
+            debugger.HandleCommand("c")
+        elif process.GetState() == lldb.eStateExited:
+           print("process exited")
+           sys.exit(1)
+        else:
+            print("Unknown status")
+            print(process.GetState())
+        time.sleep(1)
+         
+if __name__ == "__main__":
+    main()
 
 
 
